479fcae61554af4bd9d31ee463d1d8e4fbee3dc9
Viktor Balonkin <v.balonkin@gmail.com>
2026-02-04 16:01:07 +0300

    init celery beat

--- metrics/tasks.py
+++ metrics/tasks.py
@@ -1,8 +1,34 @@
+from pathlib import Path
 from celery import shared_task
+from django.utils import timezone
 
-from metrics.models import MetricRecord
+from metrics.models import Metric, MetricRecord
+
+
+REPORT_DIR = Path(__file__).resolve().parent.parent / "reports"
+REPORT_FILE = REPORT_DIR / "metrics_report.txt"
 
 
 @shared_task
-def update_metric_records_name(metric_id, new_name):
+def update_metric_records_name(metric_id: int, new_name: str):
+    """Обновляет поле name_metric"""
     MetricRecord.objects.filter(metric_id=metric_id).update(metric_name=new_name)
+
+
+
+@shared_task
+def generate_report():
+    """Генерация отчета по метрикам и записям"""
+    REPORT_DIR.mkdir(exist_ok=True)
+
+    metrics_count = Metric.objects.count()
+    records_count = MetricRecord.objects.count()
+    timestamp = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    content = (
+        f"[{timestamp}] Отчёт по метрикам:\n"
+        f"Всего метрик: {metrics_count}\n"
+        f"Всего записей: {records_count}\n"
+    )
+
+    REPORT_FILE.write_text(content, encoding="utf-8")
--- tatneft_metrics/celery.py
+++ tatneft_metrics/celery.py
@@ -6,3 +6,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tatneft_metrics.settings")
 app = Celery("tatneft_metrics")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "generate-report": {
+        "task": "metrics.tasks.generate_report",
+        "schedule": 2 * 60, 
+    },
+}
